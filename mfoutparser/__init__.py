3f0cb5c88052a34706cfca2f6d9bcbda0d7c8e19
Michelle Gill <github@michellelynngill.com>
2015-12-06 11:41:57 -0500

    Install file updates

--- mfoutparser/__init__.py
+++ mfoutparser/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-__all__ = ["parse_mfout", "write_all_to_file", "write_correlation_matrix_to_file",
-           "make_correlation_matrices", "get_data_selection"]
+__all__ = [u"parse_mfout", u"write_all_to_file", u"write_correlation_matrix_to_file",
+           u"make_correlation_matrices", u"get_data_selection"]
            
 from .read import parse_mfout
 from .write import write_all_to_file, write_correlation_matrix_to_file
@@ -9,6 +9,9 @@ from .selector import get_data_selection
 
 from .docstring import DOCSTRING
 from .version import VERSION
+
+DOCSTRING = DOCSTRING + u'\n' + \
+            u'See the `examples` directory in {:s} for a demonstration.'.format(__path__[0])
 
 __doc__ = DOCSTRING
 __version__ = VERSION
